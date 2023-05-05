b3a27b2052255ab3d433d819bfc9e0b34f9f3ac9
DylanAraujo <daraujo2@huskers.unl.edu>
2023-05-05 00:03:48 -0500

    added task app functionality

--- task.php
+++ task.php
@@ -1,5 +1,10 @@
-<!-- header.html -->
 <!DOCTYPE html>
+<html>
+<head>
+	<title>Todo List</title>
+    <link rel="stylesheet" href="css/header.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
 <html>
     <head>
         <link rel="stylesheet" href="css/header.css">
@@ -47,4 +52,67 @@
             <a href="task.php">Task App</a>
         </div>
     </nav>
+</html>
+
+
+<body>
+	<h1>Todo List</h1>
+    <form method="POST" class="flexcontainer1">
+	<label for="task">Add Task:</label>
+	<input type="text" id="task" name="task">
+	<button type="submit" class="orangecurvebutton">Add</button>
+</form>
+
+<div class="nicediv">
+	<h2>In Progress</h2>
+	<?php
+		if ($_POST) {
+			$task = $_POST['task'];
+			$file = fopen("tasks.txt", "a");
+			fwrite($file, "in progress:$task\n");
+			fclose($file);
+		}
+
+		if ($_GET) {
+			$id = $_GET['id'];
+
+			$lines = file("tasks.txt");
+			$line = $lines[$id];
+
+			if (strpos($line, "in progress:") === 0) {
+				$line = str_replace("in progress:", "done:", $line);
+				$lines[$id] = $line;
+				file_put_contents("tasks.txt", implode("", $lines));
+			}
+		}
+
+		$tasks = file("tasks.txt");
+		echo "<ul>";
+		foreach ($tasks as $id => $task) {
+			$task = str_replace("\n", "", $task);
+			if (strpos($task, "in progress:") === 0) {
+				$task = str_replace("in progress:", "", $task);
+				echo "<li>$task <a href='?id=$id' class='orangebackcurved'>[DONE]</a></li>";
+			}
+		}
+		echo "</ul>";
+	?>
+</div>
+
+<div class="nicediv">
+	<h2>Completed Tasks</h2>
+	<?php
+		$tasks = file("tasks.txt");
+		echo "<ul>";
+		foreach ($tasks as $id => $task) {
+			$task = str_replace("\n", "", $task);
+			if (strpos($task, "done:") === 0) {
+				$task = str_replace("done:", "", $task);
+				echo "<li>$task</li>";
+			}
+		}
+		echo "</ul>";
+	?>
+</div>
+</body>
 </html>
